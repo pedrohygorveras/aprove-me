f9ed918b6d3ee5881fa55f43e61a9d19f8846415
pedrohygorveras <pedrohygorveras@gmail.com>
2024-11-13 08:40:57 -0300

    add: add loading component for asynchronous operations

---
+++ project/front-end/src/components/loading/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading: React.FC = () => {
+  return (
+    <div className="py-8 text-center">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
+};
+
+export { Loading };
