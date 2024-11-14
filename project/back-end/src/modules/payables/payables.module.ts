61e009d96d822040a3ee040b9c639c23a9d39372
pedrohygorveras <pedrohygorveras@gmail.com>
2024-11-14 15:45:59 -0300

    chore: update PayablesModule configuration

--- project/back-end/src/modules/payables/payables.module.ts
+++ project/back-end/src/modules/payables/payables.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { PayablesService } from './payables.service';
 import { PayablesController } from './payables.controller';
 import { PrismaService } from '../../database/prisma.service';
+import { PayableProcessor } from './payable.processor';
+import { UsersService } from '../users/users.service';
+import { DeadLetterQueuesService } from '../dead-letter-queues/dead-letter-queues.service';
+import { EmailService } from './email.service';
 
 @Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'payablesExchange',
+          type: 'topic',
+        },
+      ],
+      uri: process.env.RABBITMQ_URI,
+      connectionInitOptions: { wait: false },
+    }),
+  ],
   controllers: [PayablesController],
-  providers: [PayablesService, PrismaService],
+  providers: [
+    PayablesService,
+    PrismaService,
+    PayableProcessor,
+    DeadLetterQueuesService,
+    EmailService,
+    UsersService,
+  ],
   exports: [PayablesService],
 })
 export class PayablesModule {}
